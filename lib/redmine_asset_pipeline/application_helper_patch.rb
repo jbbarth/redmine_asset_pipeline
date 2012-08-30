fb47dfbe8d05c4b8d636e18ae145ad261b671528
jbbarth <jeanbaptiste.barth@gmail.com>
2012-08-30 09:01:31 +0200

    Fixed that Redmine theme were not included magically anymore

--- lib/redmine_asset_pipeline/application_helper_patch.rb
+++ lib/redmine_asset_pipeline/application_helper_patch.rb
@@ -3,7 +3,13 @@ require_dependency 'application_helper'
 module ApplicationHelper
   def stylesheet_link_tag(*sources)
     return '' if sources.last.is_a?(Hash) && sources.last.delete(:plugin)
-    super
+    #magically find current redmine theme if any defined
+    #simplified version of normal version, since the magic is only used in core layout
+    if current_theme && current_theme.stylesheets.include?(sources.first)
+      super(current_theme.stylesheet_path(sources.first))
+    else
+      super
+    end
   end
 
   def javascript_include_tag(*sources)
